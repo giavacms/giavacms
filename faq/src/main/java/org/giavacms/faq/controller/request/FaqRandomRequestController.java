7e3edc461b427f6cbfc40f4325b33ce3415723ca
pisi79 <samuele.pasini@gmail.com>
2013-06-11 11:22:26 +0200

    random and co

---
+++ faq/src/main/java/org/giavacms/faq/controller/request/FaqRandomRequestController.java
@@ -0,0 +1,36 @@
+package org.giavacms.faq.controller.request;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.Random;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.giavacms.common.annotation.OwnRepository;
+import org.giavacms.faq.model.Faq;
+import org.giavacms.faq.repository.FaqRepository;
+
+@Named
+@RequestScoped
+public class FaqRandomRequestController
+         extends FaqRequestController implements Serializable
+{
+
+   private static final long serialVersionUID = 1L;
+
+   Random random = new Random();
+
+   @Inject
+   @OwnRepository(FaqRepository.class)
+   FaqRepository faqRepository;
+
+   @Override
+   public List<Faq> loadPage(int startRow, int pageSize)
+   {
+      int size = faqRepository.getListSize(getSearch());
+      startRow = random.nextInt(size);
+      return super.loadPage(startRow, pageSize);
+   }
+}
