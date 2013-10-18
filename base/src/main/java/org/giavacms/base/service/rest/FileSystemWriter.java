678c7c2184bcf83c724ab064102a5d61b3265d44
fiorenzo pizza <fiorenzino@gmail.com>
2013-10-18 14:51:48 +0200

    aggiunta la cache

--- base/src/main/java/org/giavacms/base/service/rest/FileSystemWriter.java
+++ base/src/main/java/org/giavacms/base/service/rest/FileSystemWriter.java
@@ -1,7 +1,6 @@
 package org.giavacms.base.service.rest;
 
 import java.io.Serializable;
-import java.util.List;
 
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
@@ -11,10 +10,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
-import org.giavacms.base.model.Page;
-import org.giavacms.base.repository.PageRepository;
-import org.giavacms.base.service.FileSystemWriterService;
-import org.giavacms.common.model.Search;
+import org.giavacms.base.service.CacheService;
 import org.jboss.logging.Logger;
 
 @Path("/v1/writer")
@@ -28,10 +24,7 @@ public class FileSystemWriter implements Serializable
    Logger logger = Logger.getLogger(getClass().getCanonicalName());
 
    @Inject
-   FileSystemWriterService fileSystemWriterService;
-
-   @Inject
-   PageRepository pageRepository;
+   CacheService cahCacheService;
 
    @GET
    @Path("/write/{page}")
@@ -39,26 +32,7 @@ public class FileSystemWriter implements Serializable
    public String write(
             @PathParam("page") String pageId)
    {
-      try
-      {
-         StringBuffer sb = new StringBuffer();
-         boolean fetch = true;
-         Page page = new Page();
-         page.setId(pageId);
-         boolean overwrite = true;
-         String path = null;
-         List<String> files = fileSystemWriterService.write(path, page, fetch, overwrite);
-         for (String file : files)
-         {
-            sb.append(", ").append(file);
-         }
-         return "Written: " + (sb.length() == 0 ? "none" : sb.substring(1));
-      }
-      catch (Exception e)
-      {
-         e.printStackTrace();
-         return e.getClass().getCanonicalName() + ": " + e.getMessage();
-      }
+      return cahCacheService.write(pageId);
    }
 
    @GET
@@ -66,8 +40,7 @@ public class FileSystemWriter implements Serializable
    @Produces("text/plain")
    public String writeAll()
    {
-      Search<Page> sp = new Search<Page>(Page.class);
-      return wrteAllWithSearch(sp, true);
+      return cahCacheService.writeAll();
    }
 
    @GET
@@ -75,39 +48,8 @@ public class FileSystemWriter implements Serializable
    @Produces("text/plain")
    public String writeByTemplate(@PathParam("id") Long id)
    {
-      Search<Page> sp = new Search<Page>(Page.class);
-      sp.getObj().getTemplate().getTemplate().setId(id);
-      return wrteAllWithSearch(sp, true);
-   }
 
-   private String wrteAllWithSearch(Search<Page> search, boolean overwrite)
-   {
-      try
-      {
-         StringBuffer sb = new StringBuffer();
-         boolean fetch = false;
-         String path = null;
-         int pages = pageRepository.getListSize(search);
-         int pagesPerIteration = 10;
-         for (int i = 0; i < pages; i = i + pagesPerIteration)
-         {
-            for (Page page : pageRepository.getList(new Search<Page>(Page.class), i, pagesPerIteration))
-            {
-               List<String> files = fileSystemWriterService.write(path, page, fetch, overwrite);
-               for (String file : files)
-               {
-                  sb.append(", ").append(file);
-               }
-            }
-            sb.append("---------------------------------------");
-         }
-         return "Written: " + (sb.length() == 0 ? "none" : sb.substring(1));
-      }
-      catch (Exception e)
-      {
-         e.printStackTrace();
-         return e.getClass().getCanonicalName() + ": " + e.getMessage();
-      }
+      return cahCacheService.writeByTemplate(id);
    }
 
    @GET
@@ -116,17 +58,7 @@ public class FileSystemWriter implements Serializable
    public String clear(
             @PathParam("page") String page)
    {
-      try
-      {
-         String path = null;
-
-         return fileSystemWriterService.clear(path, page);
-      }
-      catch (Exception e)
-      {
-         e.printStackTrace();
-         return e.getClass().getCanonicalName() + ": " + e.getMessage();
-      }
+      return cahCacheService.clear(page);
    }
 
    @GET
@@ -135,17 +67,7 @@ public class FileSystemWriter implements Serializable
    public String clearAll(
             )
    {
-      try
-      {
-         String path = null;
-
-         return fileSystemWriterService.clearAll(path);
-      }
-      catch (Exception e)
-      {
-         e.printStackTrace();
-         return e.getClass().getCanonicalName() + ": " + e.getMessage();
-      }
+      return cahCacheService.clearAll();
    }
 
    @GET
@@ -154,15 +76,7 @@ public class FileSystemWriter implements Serializable
    public String clearByTemplateId(
             @PathParam("id") Long id)
    {
-      try
-      {
-         return "TODO";
-      }
-      catch (Exception e)
-      {
-         e.printStackTrace();
-         return e.getClass().getCanonicalName() + ": " + e.getMessage();
-      }
+      return cahCacheService.clearByTemplateId(id);
    }
 
 }
