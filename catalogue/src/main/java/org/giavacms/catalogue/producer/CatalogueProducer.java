2346aa72b1acf1808ef27bd4b6ff28911ed94c15
pisi79 <samuele.pasini@gmail.com>
2014-02-14 13:50:26 +0100

    featured catalogue

--- catalogue/src/main/java/org/giavacms/catalogue/producer/CatalogueProducer.java
+++ catalogue/src/main/java/org/giavacms/catalogue/producer/CatalogueProducer.java
@@ -7,7 +7,9 @@
 package org.giavacms.catalogue.producer;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.annotation.PostConstruct;
@@ -18,7 +20,9 @@ import javax.inject.Inject;
 import javax.inject.Named;
 
 import org.giavacms.catalogue.model.Category;
+import org.giavacms.catalogue.model.Feature;
 import org.giavacms.catalogue.repository.CategoryRepository;
+import org.giavacms.catalogue.repository.FeatureRepository;
 import org.giavacms.common.model.Search;
 import org.giavacms.common.util.JSFUtils;
 import org.jboss.logging.Logger;
@@ -33,6 +37,8 @@ public class CatalogueProducer implements Serializable
 
    @Inject
    private CategoryRepository categoryRepository;
+   @Inject
+   private FeatureRepository propertyRepository;
 
    @SuppressWarnings("rawtypes")
    private Map<Class, SelectItem[]> items = null;
@@ -53,6 +59,56 @@ public class CatalogueProducer implements Serializable
                   "nessuna categoria", "seleziona categoria..."));
       }
       return items.get(Category.class);
+   }
+
+   @Produces
+   @Named
+   public SelectItem[] getFeatureItems()
+   {
+      if (items.get(Feature.class) == null)
+      {
+         List<String> propertyNames = propertyRepository.getRefereanceableNames();
+         List<SelectItem> selectItems = new ArrayList<SelectItem>();
+         selectItems.add(new SelectItem(null, "proprieta' ..."));
+         for (String propertyName : propertyNames)
+         {
+            if (propertyName != null && propertyName.trim().length() > 0)
+            {
+               selectItems.add(new SelectItem(propertyName));
+            }
+         }
+         if (selectItems.size() > 1)
+         {
+            items.put(Feature.class, selectItems.toArray(new SelectItem[] {}));
+         }
+         else
+         {
+            items.put(Feature.class, new SelectItem[] { new SelectItem(null, "nessuna proprieta'") });
+         }
+      }
+      return items.get(Feature.class);
+   }
+
+   public SelectItem[] getOptions(String featureName)
+   {
+      List<String> options = propertyRepository.getOptions(featureName);
+      List<SelectItem> selectItems = new ArrayList<SelectItem>();
+      selectItems.add(new SelectItem(null, "..."));
+      for (String option : options)
+      {
+         if (option != null && option.trim().length() > 0)
+         {
+            selectItems.add(new SelectItem(option));
+         }
+      }
+      if (selectItems.size() > 1)
+      {
+         return selectItems.toArray(new SelectItem[] {});
+      }
+      else
+      {
+         return new SelectItem[] { new SelectItem(null, "nessuna opzione disponibile") };
+      }
    }
 
    @SuppressWarnings("rawtypes")
