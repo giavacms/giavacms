fd591f4c2c4772e95877c330fb13ea218b1f70a2
fiorenzo pizza <fiorenzino@gmail.com>
2014-05-23 11:42:22 +0200

    vai su

--- catalogue-rs/src/main/java/org/giavacms/catalogue/rest/CatalogueConfigurator.java
+++ catalogue-rs/src/main/java/org/giavacms/catalogue/rest/CatalogueConfigurator.java
@@ -6,10 +6,12 @@ import javax.inject.Inject;
 
 import org.giavacms.base.rest.RsMixinConfigurator;
 import org.giavacms.catalogue.model.Product;
+import org.jboss.logging.*;
 
 @Singleton
 public class CatalogueConfigurator {
 
+	Logger logger = Logger.getLogger(getClass());
 	@Inject
 	RsMixinConfigurator rsMixinConfigurator;
 
@@ -18,6 +20,7 @@ public class CatalogueConfigurator {
 
 	@PostConstruct
 	public void init() {
+		logger.info("add module");
 		rsMixinConfigurator.getObjectMapper().getSerializationConfig()
 				.addMixInAnnotations(Product.class, ProductMixin.class);
 
