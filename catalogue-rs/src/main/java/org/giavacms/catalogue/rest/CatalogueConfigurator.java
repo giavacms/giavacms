a22bec88db1323184060662aac32306bcaa3386d
fiorenzo pizza <fiorenzino@gmail.com>
2014-05-23 11:49:23 +0200

    @Startup

--- catalogue-rs/src/main/java/org/giavacms/catalogue/rest/CatalogueConfigurator.java
+++ catalogue-rs/src/main/java/org/giavacms/catalogue/rest/CatalogueConfigurator.java
@@ -2,13 +2,15 @@ package org.giavacms.catalogue.rest;
 
 import javax.annotation.PostConstruct;
 import javax.ejb.Singleton;
+import javax.ejb.Startup;
 import javax.inject.Inject;
 
 import org.giavacms.base.rest.RsMixinConfigurator;
 import org.giavacms.catalogue.model.Product;
-import org.jboss.logging.*;
+import org.jboss.logging.Logger;
 
 @Singleton
+@Startup
 public class CatalogueConfigurator {
 
 	Logger logger = Logger.getLogger(getClass());
